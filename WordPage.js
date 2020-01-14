2dbc8188d3b411ac961cb5e9999e726ea173e3c8
itsoladayo <oladayogn@gmail.com>
2020-01-14 14:30:00 -0800

    styling for the translator page

--- WordPage.js
+++ WordPage.js
@@ -11,45 +11,68 @@ class WordPageScreen extends React.Component {
     }
     render() {
         const params = this.props.navigation.state.params
-        return (<View style={styles.container}>
-            <View style={styles.englishWordContainer}>
-                <Text style={styles.englishTitle}>{params.english}</Text>
-            </View>
-            <View style={styles.krioluWordContainer}>
-                <Text style={styles.krioluTitle}>{params.kriolu}</Text>
-            </View>
+        return (
+
+            <View style={styles.container}>
+                <View style={styles.englishWordContainer}>
+                    <Text style={styles.englishTitle}>{params.english}</Text>
+                </View>
+                <View style={styles.krioluWordContainer}>
+                    <View style={styles.krioluTopThird}></View>
+                    <View style={styles.krioluMiddle}>
+                        <Text style={styles.krioluTitle}>{params.kriolu}</Text>
+                    </View>
+                    <View style={styles.krioluLastThird}></View>
+
+                </View>
 
 
-        </View>)
+            </View>
+        )
     }
 
 }
 
 
 const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#003893'
+    },
     container: {
-        flex: 0.5,
-        backgroundColor: 'purple'
-        // justifyContent: 'center',
-        // alignItems: 'center'
+        flex: 1,
+        backgroundColor: '#003893',
     },
     englishWordContainer: {
-        flex: 0.5,
-        backgroundColor: 'red'
+        flex: 0.25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#003893'
     },
     krioluWordContainer: {
-        flex: 0.5
+        flex: 0.25,
+        backgroundColor: '#FFFFFF',
+        color: '#F7D116',
     },
+    krioluTopThird: {
+        flex: 0.33
+    },
+    krioluMiddle: {
+        flex: 0.33,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#CF2027'
+
+    },
+
     englishTitle: {
         fontWeight: "bold",
         fontSize: 30,
-
+        color: '#F7D116',
     },
     krioluTitle: {
         fontWeight: "bold",
         fontSize: 30,
-        color: "blue"
-
+        color: "#FFFFFF"
 
     },
 })
